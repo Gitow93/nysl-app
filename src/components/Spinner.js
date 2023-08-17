13f27fec2fa2c952a4cec966bcf538dd137e0063
Jorge Jiménez <jorge.jimenezmunoz.93@gmail.com>
2023-08-17 13:06:58 +0200

    create spinner

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/css/spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="page-spinner-overlay">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
