b795d2badfb4dbb9e045b83165b40a91b0cb0c6a
Jorge Jiménez <jorge.jimenezmunoz.93@gmail.com>
2023-08-21 12:30:25 +0200

    modify template

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,5 +1,4 @@
 import Header from "./Header";
-import nysl_logo from "./../assets/images/nysl_logo.png";
 import "./../assets/css/template.css";
 import LoginButton from "./LoginButton";
 
@@ -8,11 +7,6 @@ const Template = ({ title, children }) => {
     <>
       <Header />
       <div className="subheader">
-        <img
-          className="logo"
-          src={nysl_logo}
-          alt="northside youth soccer league logo"
-        />
         <h2 className="title">{title}</h2>
       </div>
       <div className="content">
